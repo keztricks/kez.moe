ef0cc23c60cf5f8b2c0c92538d4b885387761ad3
Kieran Phillips <keztricks@gmail.com>
2018-05-18 08:49:33 +0100

    Remove logging from masthead scroll JS

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function() {
-
     $("#trigger-popup").click(function() {
         var popupId = $(this).data("popup-trigger");
         $("[data-popup='" + popupId +"']").removeClass('hidden');
