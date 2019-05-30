cc39aaafa2f0d4ef51299b554cd78b375cee2035
Kieran Phillips <keztricks@gmail.com>
2019-05-30 16:21:35 +0100

    stop scrolling of page behind overlays

--- js/popup.js
+++ js/popup.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
     $(".trigger-popup").click(function() {
         var popupId = $(this).data("popup-trigger");
         $("[data-popup='" + popupId +"']").removeClass('hidden');
+        $("body").css("overflow", "hidden");
     });
     $(".close-popup").click(function() {
+        $("body").css("overflow", "visible");
         $(".popup").addClass('hidden');
     });
 });
